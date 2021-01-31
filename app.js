4574698c8843a2aaa06f0b0e49ca114180853884
mukeunzi <eunji057@gmail.com>
2021-01-31 18:50:07 +0900

    Feat: API prefix 설정
    - /api 를 기본 prefix로 설정함

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/api", indexRouter);
 
 app.use(function (req, res, next) {
 	next(createError(404));
