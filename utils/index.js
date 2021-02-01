67c0c1c00f951e2441fdd29d93968b1c0bc97571
mukeunzi <eunji057@gmail.com>
2021-02-02 02:40:32 +0900

    Feat: 파일명, 날짜 관련 유틸 생성
    - getFileName() : aws url에서 파일 명을 추출하는 메소드
    - getFileExtension() : 파일명에서 확장자를 추출하는 메소드
    - getDate() : 현재 시간을 'YYYY-MM-DD-hh-mm-ss' 형식으로 생성하는 메소드

--- utils/index.js
+++ utils/index.js
@@ -6,4 +6,26 @@ const timestampOnlyUUID = () => {
 	return uuid;
 };
 
-module.exports = { timestampOnlyUUID };
+const getFileName = (fileAccessUrl) => {
+	const regExp = /amazonaws.com\//g;
+	const match = regExp.exec(fileAccessUrl);
+	if (!match) return 0;
+
+	const startIndex = regExp.lastIndex;
+	const fileName = fileAccessUrl.substring(startIndex);
+	return fileName;
+};
+
+const getFileExtension = (fileName) => {
+	const nameEndIndex = fileName.lastIndexOf(".");
+	const extension = fileName.substring(nameEndIndex);
+	return extension;
+};
+
+const getDate = () => {
+	const [date, time] = new Date().toISOString().replace("T", " ").substring(0, 19).split(" ");
+	const result = `${date}-${time.split(":").join("-")}`;
+	return result;
+};
+
+module.exports = { timestampOnlyUUID, getFileName, getFileExtension, getDate };
