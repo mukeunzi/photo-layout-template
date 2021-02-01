1f22c294fcdd634b78f428116c0d4e5243cee29f
mukeunzi <eunji057@gmail.com>
2021-02-01 15:35:33 +0900

    Feat: AWS S3 파일 업로드 메소드 구현
    - aws 연동을 위해 aws-sdk 모듈 설치
    - multipart/form-data 업로드를 위해 multer, multer-s3 모듈 설치
    - 지정한 S3 버킷에 파일을 업로드하는 메소드 구현
    - `{날짜}-{사용자지정파일명}.{확장자}` 형식의 파일명 지정

---
+++ utils/aws.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+
+AWS.config.update({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+const upload = multer({
+	storage: multerS3({
+		s3,
+		bucket: process.env.AWS_S3_BUCKET_NAME,
+		acl: "public-read",
+		metadata: function (req, file, cb) {
+			cb(null, { fieldName: file.fieldname });
+		},
+		key: function (req, file, cb) {
+			const extension = path.extname(file.originalname);
+			const fileName = encodeURIComponent(path.basename(file.originalname, extension));
+			cb(null, `${Date.now()}-${fileName}${extension}`);
+		},
+	}),
+});
+
+module.exports = { upload };
