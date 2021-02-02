556404480ee0d175987b84364b83217741932c5d
mukeunzi <eunji057@gmail.com>
2021-02-02 17:49:35 +0900

    Feat: 통합 테스트 환경 세팅
    - jest, supertest 모듈 설치
    - 테스트 실행을 위한 `npm test` 명령어 추가
    - db test 환경변수 설정

--- config/config.js
+++ config/config.js
@@ -9,10 +9,10 @@ module.exports = {
 		dialect: "mysql",
 	},
 	test: {
-		username: "root",
-		password: null,
-		database: "database_test",
-		host: "127.0.0.1",
+		username: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DB,
+		host: process.env.MYSQL_HOST,
 		dialect: "mysql",
 	},
 	production: {
